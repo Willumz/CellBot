44c6a16d838ef9dc832db0f92bc004d6e9866b29
William <15824869+Willumz@users.noreply.github.com>
2018-12-10 23:37:52 +0000

    Made prefix universal
    Each command module is now a function. When it is included, it is passed the prefix, and it returns all its properties as it previously did. The difference is these properties can dynamically change based on the prefix before they are returned, allowing their usage docs to include the correct prefix.

--- commands/userembed.js
+++ commands/userembed.js
@@ -1,25 +1,30 @@
 const discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-  let embed = new discord.RichEmbed()
-    .setAuthor(message.author.username)
-    .setDescription("This is the user's info.")
-    .setColor("#FFFFFF")
-    .addField(
-      "Full Username",
-      `${message.author.username}#${message.author.discriminator}`
-    )
-    .addField("ID", message.author.id)
-    .addField("Created At", message.author.createdAt);
-  message.channel.send({ embed: embed });
-};
+module.exports = function(prefix) {
+  var exports = {};
+  exports.run = async (bot, message, args) => {
+    let embed = new discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setDescription("This is the user's info.")
+      .setColor("#FFFFFF")
+      .addField(
+        "Full Username",
+        `${message.author.username}#${message.author.discriminator}`
+      )
+      .addField("ID", message.author.id)
+      .addField("Created At", message.author.createdAt);
+    message.channel.send({ embed: embed });
+  };
 
-module.exports.verify = message => {
-  return true;
-};
+  exports.verify = message => {
+    return true;
+  };
 
-module.exports.info = {
-  name: "User Embed",
-  command: "user",
-  usage: "!user"
+  exports.info = {
+    name: "User Embed",
+    command: "user",
+    usage: `${prefix}user`
+  };
+
+  return exports;
 };
